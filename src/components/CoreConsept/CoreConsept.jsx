ccc43fca082c9fd600e8fa276613f1627a8e5c1c
Andrew <herobrinemakcimov@gmail.com>
2024-09-20 18:00:36 +0200

    added-list-buttons

---
+++ src/components/CoreConsept/CoreConsept.jsx
@@ -0,0 +1,11 @@
+import "./CoreConsept.css";
+
+export default function CoreConsept({ image, title, description }) {
+  return (
+    <li>
+      <img src={image} alt={title} />
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
